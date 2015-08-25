63c98ad71fe7ed8cb9ce2196e84e29773bd0d724
ryonext <ryonext.s@gmail.com>
2015-08-25 21:45:07 +0900

    Fix csv file name.

--- app/controllers/gears_controller.rb
+++ app/controllers/gears_controller.rb
@@ -3,16 +3,21 @@ class GearsController < ApplicationController
   # GET /gears.json
   def index
     @gears = Gear.where(part: Gear.parts[part_param])
+    @part = part_param
   end
 
   def csv
     @gears = Gear.where(id: params[:gear][:id])
-    send_data render_to_string, filename: "gear.csv", type: :csv
+    send_data render_to_string, filename: csv_file_name, type: :csv
   end
 
   private
     def part_param
       return :head unless params[:part]
       params[:part]
+    end
+
+    def csv_file_name
+      "#{Time.zone.now}_#{part_param}.csv"
     end
 end
