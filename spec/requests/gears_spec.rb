f4b84e7ee085d62e258fb75ce4cab0b13985aa28
ryonext <ryonext.s@gmail.com>
2015-08-29 21:05:07 +0900

    Fix csv bug.

--- spec/requests/gears_spec.rb
+++ spec/requests/gears_spec.rb
@@ -7,4 +7,15 @@ RSpec.describe "Gears", type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe "POST /gears/csv" do
+    let(:gear){ Gear.create!(name: "hoge", part: "head", bland_id: bland.id, gear_power_id: gear_power.id) }
+    let(:bland){ Bland.create!(name: "hoge_bland") }
+    let(:gear_power){ GearPower.create!(name: "hoge_power") }
+
+    it "works! (now write some real specs)" do
+      post csv_gears_path, { gear: { id: [gear.id] } }
+      expect(response).to have_http_status(200)
+    end
+  end
 end
